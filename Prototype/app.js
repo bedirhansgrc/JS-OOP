6ebd93dc8a88d2cb608dab00daee043dc4a8f741
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-08-18 17:24:25 +0300

    Update app.js

--- Prototype/app.js
+++ Prototype/app.js
@@ -1,10 +1,26 @@
-function Employee(name, age) {
+// function Employee(name, age) {
+//     this.name = name
+//     this.age = age
+//     this.showInfos = function(){
+//         ("Bilgiler Gösteriliyor")
+//     }
+// }
+
+// const emp1 = new Employee("Bedo",22)
+// console.log(emp1)
+
+function Employee(name,age){
     this.name = name
     this.age = age
-    this.showInfos = function(){
-        ("Bilgiler Gösteriliyor")
-    }
+    // this.showInfos = function(){
+    //     console.log("İsim : " + this.name + "Yaş : " + this.age)
+    // }
+}
+
+Employee.prototype.showInfos = function(){
+    console.log("İsim : " + this.name + "Yaş :" + this.age)
 }
 
 const emp1 = new Employee("Bedo",22)
+emp1.showInfos()
 console.log(emp1)
